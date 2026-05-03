295dda220728e2be0a0fd5df30c63dd74504978a
kaustubh Satish patil <kaustubhsatishpatil@kaustubhs-MacBook-Air.local>
2026-05-03 21:02:06 -0230

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,11 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://backendreviewtool.onrender.com',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8000',
+        target: 'https://backendreviewtool.onrender.com',
         changeOrigin: true,
       },
     },
